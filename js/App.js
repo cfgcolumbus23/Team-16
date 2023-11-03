2ed3a2d7c2e7d3a4a91289ba0fe50387c9ddbe82
Shritan Gunday <gunday.1@buckeyemail.osu.edu>
2023-11-03 17:26:56 -0400

    Made skeleton components and categorized folders

---
+++ js/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+<Homepage>
+    <Login>
+        <Parent>
+            <Scores></Scores>
+            <Guide></Guide>
+            <Children></Children>
+            <Referrals></Referrals>
+        </Parent>
+    </Login>
+</Homepage>
