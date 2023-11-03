185342e01fe81cc9e07cb5c910b1e454acfff07f
khushipatel2 <khushinp2912@gmail.com>
2023-11-03 19:18:18 -0400

    Setup parent and org models

--- fr5backend/request_data/models.py
+++ fr5backend/request_data/models.py
@@ -6,11 +6,26 @@ from django.db import models
 # child
 
 # parent
+class Parent(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=150)
+    phone_number = models.CharField(max_length=10)
+    address = models.CharField(max_length=150)
+
 
 # intervention guide
 
 # assessment
 
 # organization
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    phone_number = models.CharField(max_length=10)
+    address = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+
 
 # admin
