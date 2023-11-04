d89a31866aae6d5964a9503dcb5abe3e7a8b4d6e
rahmedd <rahmedd@umich.edu>
2023-11-04 04:58:32 -0400

    Delete component feature for organization, fix styles

--- cfg23/src/childbar.jsx
+++ cfg23/src/childbar.jsx
@@ -1,36 +1,50 @@
 import React from "react";
+import "./styles/normalize.css";
+import "./Create.css";
+import "./styles/scoresview.css";
+import "./styles/profileview.css";
+import "./styles/referralview.css";
 import profile_pic from "./assets/profile.png";
-import "./childbar.css";
 
 function ChildBar({ handleImageClick }) {
   const children = [
-    profile_pic,
-    profile_pic,
-    profile_pic,
-    profile_pic,
-    profile_pic,
-    profile_pic,
-    profile_pic,
-    profile_pic,
-    profile_pic,
+    { img: profile_pic, name: "Child 1 Name", info: "Information" },
+    { img: profile_pic, name: "Child 2 Name", info: "Information" },
+    { img: profile_pic, name: "Child 3 Name", info: "Information" },
+    { img: profile_pic, name: "Child 4 Name", info: "Information" },
+    { img: profile_pic, name: "Child 5 Name", info: "Information" },
+    { img: profile_pic, name: "Child 6 Name", info: "Information" },
+    { img: profile_pic, name: "Child 7 Name", info: "Information" },
+    { img: profile_pic, name: "Child 8 Name", info: "Information" },
   ];
 
   const handleClick = (index) => {
     console.log(`Image clicked: ${index}`);
     handleImageClick(index);
   };
-
-  const output_images = () =>
-    children.map((child, index) => (
-      <img
-        src={child}
-        alt={`Child ${index}`}
-        key={index}
-        onClick={() => handleClick(index)}
-      ></img>
-    ));
-
-  return <div className="child-list">{output_images()}</div>;
+  return (
+    <div className="childBar">
+      <div className="title">
+        <h3 align="center">Children</h3>
+      </div>
+      <div className="rand">
+        {children.map((child, index) => (
+          <section className="childSection" key={index}>
+            <img
+              src={child.img}
+              alt="Profile"
+              className="img"
+              onClick={() => handleClick(index)}
+            />
+            <p>{child.name}</p>
+            <div className="div">
+              <p>{child.info}</p>
+            </div>
+          </section>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ChildBar;
