d341d500729ea5fc930a63178a6d88c93a2f447b
rahmedd <rahmedd@umich.edu>
2023-11-04 00:43:42 -0400

    Create account UI. Add CSS style to page.

--- cfg23/src/App.jsx
+++ cfg23/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import Login from "./Login";
 import ParentView from "./Parent";
+import CreateAccount from "./create";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route index element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/parent" element={<ParentView />} />
+          <Route path="/create" element={<CreateAccount />} />
         </Routes>
       </BrowserRouter>
     </>
