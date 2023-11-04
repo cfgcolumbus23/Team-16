ca466e35b159a13f6903db043a5db880fc923c73
khushipatel2 <khushinp2912@gmail.com>
2023-11-03 21:52:40 -0400

    Add login model

--- fr5backend/request_data/models.py
+++ fr5backend/request_data/models.py
@@ -8,7 +8,6 @@ class Child(models.Model):
     birthdate = models.DateField()
     parent = models.ForeignKey("Parent", on_delete=models.CASCADE)
     guide = models.ForeignKey("Guide", on_delete=models.CASCADE)
-
     assessement = models.ManyToManyField("Assessment", through="AssessmentDetail")
 
 class Assessment(models.Model):
@@ -18,31 +17,29 @@ class Assessment(models.Model):
     age_group = models.FloatField()
 
 class AssessmentDetail(models.Model):
-    """An assessment detail has a literacy score, a math score, a social score, a physical score"""
+    """An assessment detail has a literacy score, a math score, a social score, a physical score, an overall score, a child, and an assessment"""
     """Assessment detail is the relationship between an assessment and a child"""
-    child = models.ForeignKey("Child", on_delete=models.CASCADE)
-    assessment = models.ForeignKey("Assessment", on_delete=models.CASCADE)
-
     literacy = models.FloatField()
     math = models.FloatField()
     social = models.FloatField()
     physical = models.FloatField()
     overall = models.FloatField()
+    child = models.ForeignKey("Child", on_delete=models.CASCADE)
+    assessment = models.ForeignKey("Assessment", on_delete=models.CASCADE)
 
 class Parent(models.Model):
-    """A parent has a first name, a last name, a username, a password, a email, a phone number, and an address"""
-    """A parent has many children"""
+    """A parent has a first name, a last name, an email, a phone number, and an address"""
+    """A parent has many children and one login"""
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
     email = models.EmailField(max_length=150)
     phone_number = models.CharField(max_length=10)
     address = models.CharField(max_length=150)
+    login = models.OneToOneField("Login", on_delete=models.CASCADE)
 
 class Organization(models.Model):
-    """An organization has a name, a email, a username, a phone number, an address, and a description"""
-    """An organization has many many children"""
+    """An organization has a name, an email, a phone number, an address, and a description"""
+    """An organization has many children"""
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=150)
     phone_number = models.CharField(max_length=10)
@@ -51,17 +48,20 @@ class Organization(models.Model):
     child = models.ManyToManyField(Child)
 
 class Guide(models.Model):
-    """A guide has a first name, a last name, a username, a password, an email, a phone number, and a description"""
-    """A guide has many children and many assessments"""
+    """A guide has a first name, a last name, an email, a phone number, and a description"""
+    """A guide has many children and many assessments and one login"""
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length = 200)
-    password = models.CharField(max_length = 200)
-    email  = models.CharField(max_length = 200)
+    email = models.CharField(max_length = 200)
     phone_number = models.CharField(max_length = 200)
     description = models.TextField()
+    login = models.OneToOneField("Login", on_delete=models.CASCADE)
 
 class Admin(models.Model):
-    """An admin has a username and a password"""
+    """An admin has one login"""
+    login = models.OneToOneField("Login", on_delete=models.CASCADE)
+
+class Login(models.Model):
+    """A login has a username and a password"""
     username = models.CharField(max_length = 200)
     password = models.CharField(max_length = 200)
