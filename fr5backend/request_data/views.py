44f8979150a1e3af03350cb05df7cb8edfa62bea
Srihari Das <sriharigd04@gmail.com>
2023-11-03 23:52:06 -0400

    Implemented ChildListApiView.

--- fr5backend/request_data/views.py
+++ fr5backend/request_data/views.py
@@ -7,3 +7,11 @@ from .models import Parent, Child, Assessment, AssessmentDetail, Organization, G
 from .serializers import ParentSerializer, ChildSerializer#, AssessmentSerializer, AssessmentDetailSerializer, OrganizationSerializer, GuideSerializer, AdminSerializer, LoginSerializer
 
 # Create your views here.
+class ChildListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+
+        children = Parent.objects.filter(parent = request.parent.id)
+        serializer = ChildSerializer(children, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
