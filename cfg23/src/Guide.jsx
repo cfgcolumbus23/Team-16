69997609432565b013cf93dbde5e762e68348144
Shritan Gunday <gunday.1@buckeyemail.osu.edu>
2023-11-04 00:42:47 -0400

    Updated Guide.jsx

--- cfg23/src/Guide.jsx
+++ cfg23/src/Guide.jsx
@@ -6,16 +6,44 @@ import profile_pic from './assets/profile.png';
 
 export default function Guide() {
 
+    const children = [
+        {profile_pic},
+        {profile_pic},
+        {profile_pic},
+        {profile_pic}
+    ]
+
+    ///console.log(document.toString());
+    // Get a reference to the image container <div>
+    const imageContainer = document.getElementById('child-list');
+
+    // Loop through the image URLs and create <img> elements
+    children.forEach((child) => {
+        ///console.log(typeof imageContainer);
+        // Create an <img> element
+        const imageElement = document.createElement('img');
+        if (imageElement != null) {
+            // Set the src attribute to the image URL
+            imageElement.src = child;
+            // Append the <img> element to the image container
+            imageContainer.appendChild(child);
+        } else {
+            console.log("unsupported");
+        }
+    });
+
+
     {/* Loops through images to display within the child list */}
-    for (let i = 0; i < 20; i++) {            
-        const element = '<img src="' + {profile_pic} + '">'
-        document.querySelector('.child-list').innerHTML += element;
-    }
+    
     return (
         <>
-            {/* Displays the children pictures in the viewer */}
-            <div className="child-list"></div>
-            <img src={profile_pic}/>
+            <div>
+                <div className="list-title">
+                    <h1>Your Students</h1>
+                </div>
+                {/* Displays the children pictures in the viewer */}
+                <div id="child-list"></div>
+            </div>
         </>
     );
 }
--- cfg23/src/Parent.jsx
+++ cfg23/src/Parent.jsx
@@ -1,109 +1,112 @@
 import React from "react";
-import profilePic from "./assets/profilePic.jpg";
+import profilePic from "./assets/profile.png";
 import "./styles/normalize.css";
 import "./styles/parent.css";
+import "./styles/scoresview.css";
+import "./styles/profileview.css";
+import "./styles/referralview.css";
 
 // Child view section
 function ChildBar() {
-    return (
-        <div className="childBar">
-            <h3>Children</h3>
-            <section className="childSection">
-                <img src={profilePic}></img>
-                <p>Child 1</p>
-            </section>  
-            <section className="childSection">
-                <img src={profilePic}></img>
-                <p>Child 2</p>
-            </section>
-            <section className="childSection">
-                <img src={profilePic}></img>
-                <p>Child 3</p>
-            </section>  
-        </div>
-    );
+  return (
+    <div className="childBar">
+      <h3>Children</h3>
+      <section className="childSection">
+        <img src={profilePic} alt="Profile Pic"></img>
+        <p>Child 1</p>
+      </section>
+      <section className="childSection">
+        <img src={profilePic} alt="Profile Pic"></img>
+        <p>Child 2</p>
+      </section>
+      <section className="childSection">
+        <img src={profilePic} alt="Profile Pic"></img>
+        <p>Child 3</p>
+      </section>
+    </div>
+  );
 }
 
 // Get a quick overview of a child's scores and categorical progress
 function ScoresView() {
-    return (
-        <div className="scoresView">
-            <h2>"Child 1's" Scores:</h2>
-            <h3>Overall Score: ---%</h3>
-            <section className="scoresSection">
-                <h3>Category 1:</h3>
-                <p>----+--</p>
-                <p>More ^</p>
-            </section>
-            <section className="scoresSection">
-                <h3>Category 2:</h3>
-                <p>----+--</p>
-                <p>More ^</p>
-            </section>
-            <section className="scoresSection">
-                <h3>Category 3:</h3>
-                <p>----+--</p>
-                <p>More ^</p>
-            </section>
-            <section className="scoresSection">
-                <h3>Category 4:</h3>
-                <p>----+--</p>
-                <p>More ^</p>
-            </section>
-        </div>
-    );
+  return (
+    <div className="scoresView">
+      <h2>"Child 1's" Scores:</h2>
+      <h3>Overall Score: ---%</h3>
+      <section className="scoresSection">
+        <h3>Category 1:</h3>
+        <p>----+--</p>
+        <p>More ^</p>
+      </section>
+      <section className="scoresSection">
+        <h3>Category 2:</h3>
+        <p>----+--</p>
+        <p>More ^</p>
+      </section>
+      <section className="scoresSection">
+        <h3>Category 3:</h3>
+        <p>----+--</p>
+        <p>More ^</p>
+      </section>
+      <section className="scoresSection">
+        <h3>Category 4:</h3>
+        <p>----+--</p>
+        <p>More ^</p>
+      </section>
+    </div>
+  );
 }
 
 // View the profile of the intervention guide
 function ProfileView() {
-    return (
-        <div className="profileView">
-            <img src={profilePic}></img>
-            <p>Name: Guide 1</p>
-            <p>Contact Info: guide1@jpmc.com</p>
-        </div>
-    );
+  return (
+    <div className="profileView">
+      <img src={profilePic} alt="Profile Pic"></img>
+      <p>Name: Guide 1</p>
+      <p>Contact Info: guide1@jpmc.com</p>
+    </div>
+  );
 }
 
 // View the programs and organizations the guide has recommended
 function ReferralView() {
-    return (
-        <div className="referralView">
-            <section className="referralSection">
-                <p>Category 1 Referrals:</p>
-                <p>Group a</p>
-                <p>Group b</p>
-                <p>Group c</p>
-            </section>
-            <section className="referralSection">
-                <p>Category 2 Referrals:</p>
-                <p>Group i</p>
-                <p>Group j</p>
-                <p>Group k</p>
-            </section>
-            <section className="referralSection">
-                <p>Category 3 Referrals:</p>
-                <p>Group q</p>
-                <p>Group r</p>
-                <p>Group s</p>
-            </section>
-            <section className="referralSection">
-                <p>Category 4 Referrals:</p>
-                <p>Group x</p>
-                <p>Group y</p>
-                <p>Group z</p>
-            </section>
-        </div>
-    );
+  return (
+    <div className="referralView">
+      <section className="referralSection">
+        <p>Category 1 Referrals:</p>
+        <p>Group a</p>
+        <p>Group b</p>
+        <p>Group c</p>
+      </section>
+      <section className="referralSection">
+        <p>Category 2 Referrals:</p>
+        <p>Group i</p>
+        <p>Group j</p>
+        <p>Group k</p>
+      </section>
+      <section className="referralSection">
+        <p>Category 3 Referrals:</p>
+        <p>Group q</p>
+        <p>Group r</p>
+        <p>Group s</p>
+      </section>
+      <section className="referralSection">
+        <p>Category 4 Referrals:</p>
+        <p>Group x</p>
+        <p>Group y</p>
+        <p>Group z</p>
+      </section>
+    </div>
+  );
 }
 
 export default function ParentView() {
-    return (
-        <div className="parentView">
-            <ChildBar />
-            <ScoresView />
-            <ProfileView />
-            <ReferralView />
-        </div>
-    );
+  return (
+    <div className="parentView">
+      <ChildBar />
+      <ScoresView />
+      <ProfileView />
+      <ReferralView />
+    </div>
+  );
 }
