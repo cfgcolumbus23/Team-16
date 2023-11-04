52f2f94e06e67e762cb6b333a07411bf8ba783ee
rahmedd <rahmedd@umich.edu>
2023-11-04 10:18:13 -0400

    Add to organize UI

--- cfg23/src/newcomponent.jsx
+++ cfg23/src/newcomponent.jsx
@@ -1,84 +1,67 @@
 import React from "react";
 import "./newcomponent.css";
-<<<<<<< HEAD
-=======
 import "./styles/parent.css";
 import line from "./assets/line.png";
 import bar from "./assets/color_bar.png";
 
 //return the child information based on which child was clicked, displaying their scores
->>>>>>> frontend
 function NewComponent({ onDelete }) {
   const handleDelete = () => {
     onDelete();
   };
+  const Edit = () => {};
   return (
-<<<<<<< HEAD
-    <div className="newcomponent">
-      <div className="scoresView">
+    <div className="scoresView">
+      <div>
         <button onClick={handleDelete}>X</button>
         <h2>"Child 1's" Scores:</h2>
         <h3>Overall Score: ---%</h3>
         <div className="scoresContainer">
-          <section className="scoresS">
+          <section className="scoresSection">
             <h3>Category 1:</h3>
-            <p>----+--</p>
+            <img className="bar" src={bar} alt="progress-bar" />
+            <div className="line">
+              <img id="line" src={line} alt="line" />
+            </div>
+            <p>More ^</p>
           </section>
-          <section className="scoresS">
+          <section className="scoresSection">
             <h3>Category 2:</h3>
-            <p>----+--</p>
+            <img className="bar" src={bar} alt="progress-bar" />
+            <div className="line">
+              <img id="line" src={line} alt="line" />
+            </div>
+            <p>More ^</p>
           </section>
-          <section className="scoresS">
+          <section className="scoresSection">
             <h3>Category 3:</h3>
-            <p>----+--</p>
+            <img className="bar" src={bar} alt="progress-bar" />
+            <div className="line">
+              <img id="line" src={line} alt="line" />
+            </div>
+            <p>More ^</p>
           </section>
-          <section className="scoresS">
+          <section className="scoresSection">
             <h3>Category 4:</h3>
-            <p>----+--</p>
+            <img className="bar" src={bar} alt="progress-bar" />
+            <div className="line">
+              <img id="line" src={line} alt="line" />
+            </div>
+            <p>More ^</p>
           </section>
         </div>
+        <br></br>
+        <div className="buttonsand">
+          <div className="edit">
+            <button onClick={Edit}>Edit Child Attendance</button>
+          </div>
+          <div className="parent-contact">
+            <h3>Parent Contact Information</h3>
+            <p>Email: parent@example.com</p>
+            <p>Phone: (123) 456-7890</p>
+          </div>
+        </div>
       </div>
-=======
-    <div className="scoresView">
-      <button onClick={handleDelete}>X</button>
-      <h2>"Child 1's" Scores:</h2>
-      <h3>Overall Score: ---%</h3>
-      <div className="scoresContainer">
-        <section className="scoresSection">
-          <h3>Category 1:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-        <section className="scoresSection">
-          <h3>Category 2:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-        <section className="scoresSection">
-          <h3>Category 3:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-        <section className="scoresSection">
-          <h3>Category 4:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-      </div>
-      <p>Parent contact information:</p>
->>>>>>> frontend
     </div>
   );
 }
