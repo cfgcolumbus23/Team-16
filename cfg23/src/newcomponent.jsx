e8e6ef0b738feff23e6fd3122ed960922c9c00a9
rahmedd <rahmedd@umich.edu>
2023-11-04 07:18:38 -0400

    Revert "login commit"
    This reverts commit d915a57385c38d46d6335b197ee6f60b5261ddf0.

--- cfg23/src/newcomponent.jsx
+++ cfg23/src/newcomponent.jsx
@@ -1,75 +1,34 @@
 import React from "react";
 import "./newcomponent.css";
-import "./styles/parent.css";
-import line from "./assets/line.png";
-import bar from "./assets/color_bar.png";
-
-function showNotesField() {
-  document.getElementById("notesField").style.display = "block";
-}
-
-//return the child information based on which child was clicked, displaying their scores
 function NewComponent({ onDelete }) {
-  //handle the event where the new component rendered wants to be deleted
   const handleDelete = () => {
     onDelete();
   };
   return (
-    <div className="scoresView">
-      <button onClick={handleDelete}>X</button>
-      <h2>"Child 1's" Scores:</h2>
-      <h3>Overall Score: ---%</h3>
-      <div className="scoresContainer">
-        <section className="scoresSection">
-          <h3>Category 1:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-        <section className="scoresSection">
-          <h3>Category 2:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-        <section className="scoresSection">
-          <h3>Category 3:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-        <section className="scoresSection">
-          <h3>Category 4:</h3>
-          <img className="bar" src={bar} alt="progress-bar" />
-          <div className="line">
-            <img id="line" src={line} alt="line" />
-          </div>
-          <p>More ^</p>
-        </section>
-      </div>
-      <p>Parent contact information:</p>
-      <form>
-        <button type="button" onclick="showNotesField()">
-          Add Note
-        </button>
-        <div id="notesField" style="display: none;">
-          <textarea
-            id="noteInput"
-            rows="4"
-            cols="50"
-            placeholder="Enter your note here..."
-          ></textarea>
-          <button type="button" onclick="saveNote()">
-            Save Note
-          </button>
+    <div className="newcomponent">
+      <div className="scoresView">
+        <button onClick={handleDelete}>X</button>
+        <h2>"Child 1's" Scores:</h2>
+        <h3>Overall Score: ---%</h3>
+        <div className="scoresContainer">
+          <section className="scoresS">
+            <h3>Category 1:</h3>
+            <p>----+--</p>
+          </section>
+          <section className="scoresS">
+            <h3>Category 2:</h3>
+            <p>----+--</p>
+          </section>
+          <section className="scoresS">
+            <h3>Category 3:</h3>
+            <p>----+--</p>
+          </section>
+          <section className="scoresS">
+            <h3>Category 4:</h3>
+            <p>----+--</p>
+          </section>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
