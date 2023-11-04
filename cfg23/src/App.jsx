b11ac4206978992afa445782bbba2211fb1a6704
rahmedd <rahmedd@umich.edu>
2023-11-04 07:14:04 -0400

    Guide and Organize UI

--- cfg23/src/App.jsx
+++ cfg23/src/App.jsx
@@ -9,10 +9,11 @@ import LoginGuide from "./LoginGuide";
 import LoginOrg from "./LoginOrg";
 import ParentView from "./Parent";
 import CreateAccount from "./Create";
-import Guide from "./Guide";
 import OrganizerView from "./organizer";
 import ParentComponent from "./organizer";
+import GuideComponent from "./Guides";
 import ChildBar from "./childbar";
+//import GuideComponents from "./Guides";
 
 //Links all components to each other, allowing for easy component change
 export default function App() {
@@ -25,10 +26,9 @@ export default function App() {
           <Route path="/loginGuide" element={<LoginGuide />} />
           <Route path="/loginOrg" element={<LoginOrg />} />
           <Route path="/parent" element={<ParentView />} />
-          <Route path="/guide" element={<Guide />} />
-          <Route path="/create" element={<CreateAccount />}/>
+          <Route path="/guide" element={<GuideComponent />}></Route>
+          <Route path="/create" element={<CreateAccount />} />
           <Route path="/organizer" element={<ParentComponent />}></Route>
-
         </Routes>
       </BrowserRouter>
     </>
