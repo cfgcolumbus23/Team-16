b8ac8d625f223a924f363044f1c6d80bfe78ce9c
khushipatel2 <khushinp2912@gmail.com>
2023-11-03 20:08:50 -0400

    Add password field to parent

--- fr5backend/request_data/models.py
+++ fr5backend/request_data/models.py
@@ -10,6 +10,7 @@ class Parent(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
     email = models.EmailField(max_length=150)
     phone_number = models.CharField(max_length=10)
     address = models.CharField(max_length=150)
