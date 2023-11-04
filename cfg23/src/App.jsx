c2ffe417de89f91519f27a8b2eed53af5ac81695
Shritan Gunday <gunday.1@buckeyemail.osu.edu>
2023-11-03 22:29:33 -0400

    Added Guide page and updated homepage

--- cfg23/src/App.jsx
+++ cfg23/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Imports the different React components to reference
 import Homepage from "./Homepage";
 import Login from "./Login";
 import Parent from "./Parent";
+import Guide from "./Guide";
 
+//Links all components to each other, allowing for easy component change
 export default function App() {
     return (
         <>
@@ -13,6 +17,7 @@ export default function App() {
                     <Route index element={<Homepage />}/>
                     <Route path="/login" element={<Login />}/>
                     <Route path="/parent" element={<Parent />}/>
+                    <Route path="/guide" element={<Guide />}/>
                 </Routes>
             </BrowserRouter>
         </>
